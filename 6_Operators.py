55799a1632047179d8978bb77614abf449d7a08c
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-06-09 10:02:56 +0530

    Update 6_Operators.py

--- 6_Operators.py
+++ 6_Operators.py
@@ -5,5 +5,14 @@ print(a + b)   #15
 print(a - b)   #9
 print(a * b)   #36
 print(a / b)   #4.0
-print(a // b)  #4
-print(a % b)   #0
+print(a // b)  #4 interger division rounded down towards -infinity
+print(a % b)   #0 modulous gives the remainder after integer division
+
+#usecase for integer division
+print() #empty line
+
+for i in range (1,4):   #loop prints integer till 3
+  print(i)              #it only uses int values for ranges so in place of 4, a/b will give error while a//b will work fine
+  
+#an expression is a combination of operators, operands or other expressions that can be interprated to some valuea.
+#here, leaving the a and b in line 1 and 2 (not being evaluated) along with i in line 14 (not being evaluated) rest all are expression
