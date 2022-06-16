a6178975eda69831264957647e2798b8874490d2
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-06-16 09:13:45 +0530

    Create 7_Strings_data_type.py

---
+++ 7_Strings_data_type.py
@@ -0,0 +1,9 @@
+         #012345678901234
+parrot = "Norweigion Blue"
+print(parrot)
+print(parrot[3]) #gives the fourth character of the string that is w because counting usually starts at 0
+ ##MINI CHALLENGE##
+# =>using the above var to print "we win" with all char in separate line using indexing then repeat the challeng using negative index
+#my solution-
+print(parrot+"\n"+parrot[3]+"\n"+parrot[4]+parrot[10]+"\n"+parrot[3]+"\n"+parrot[5]+"\n"+parrot[9])
+print(parrot+"\n"+parrot[-12]+"\n"+parrot[-11]+"\n"+parrot[-5]+"\n"+parrot[-12]+"\n"+\parrot[-6]+)
