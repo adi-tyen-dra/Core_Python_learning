3ac27f6845dd9472fedd0ff17b860c57cfa147ec
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-07-16 16:44:52 +0530

    Create 0014_For_Loop.py

---
+++ 0014_For_Loop.py
@@ -0,0 +1,9 @@
+#A for loop works by iterating over some set of values. 
+#It assigns each of the values, one by one, to one or more variables. 
+#It then executes a block of code once for each value.
+#The set of values comes from a sequence or some other iterable object.
+#iterable obj is anything than can be iterated over; sequence is also an iterable
+parrot = "Norweigen Blue" #sequence string
+
+for character in parrot:   #for loop
+    print(character)
