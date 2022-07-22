5f8f67ac1597f4be0d0763d713104eaa6c7d8b3c
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-07-22 06:05:58 +0530

    Update 0014_For_Loop.py

--- 0014_For_Loop.py
+++ 0014_For_Loop.py
@@ -30,3 +30,48 @@ print(sum([int(val) for val in values])) #edited to return sum
 # Note: in lang like c++ or java, for loops work differently 
 # as they work with a starting and ending value with a variable being incremented each time around the loop whereas,
 # in python the approch is much more flexible although similar effect from those lang can be achieved by iterating a loop over a range in python.
+
+# Ranges-
+#ranges with for loop-
+
+for i in range(1,12): #similar to slicing it doesn't iterate for the 12th time(end val) 
+  print("i is now {}".format(i)) #prints 1 to 11
+  
+for i in range(10,0,-2): #similar to slicing it has step values as well
+  print(i) # prints no. 10 to 1 with a step of 2
+
+for i in range(10): #will take 0 as the default start value and run ten times
+  print(i) #prints no.  0 to 9
+  
+#Challenge*********
+#write the prog to print all no. from 0 to 100, divisible by 7, (without using if)
+
+#sol1
+for i in range(0,101,7):
+  print(i)
+#sol2
+for in in range(101)[::7]: #it uses slicing
+  print(i)
+
+# Nested for loop-
+for i in range(1,13):  #i is not defined outside this loop
+    for j in range(1,13): #j is not defined outside this loop
+        print("{0:02} times {1:02} is {2:03} ".format(i,j,i*j))
+    print("------------------------")
+
+#mini challenge: what if?
+#case1: print("------") line is given indentation of 4 more space
+#solution-
+for i in range(1,13):
+    for j in range(1,13):
+        print("{0:02} times {1:02} is {2:03} ".format(i,j,i*j))
+        print("------------------------") #here in case1, line will be drawn after every record
+
+#case2: print("------") line is given no indentation
+#solution-
+for i in range(1,13):
+    for j in range(1,13):
+        print("{0:02} times {1:02} is {2:03} ".format(i,j,i*j))
+print("------------------------") #here in case2, line will be drawn only once at the very last
+
+# CONTINUE and BREAK keywords-
