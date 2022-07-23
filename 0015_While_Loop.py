22dc0cc06a89d47042700a9120e4bbf7ffa3bdae
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-07-23 18:37:01 +0530

    Create 0015_While_Loop.py

---
+++ 0015_While_Loop.py
@@ -0,0 +1,7 @@
+# the basic form is 
+# while <condition>: 
+#     execute block of code.
+# The condition can be anything that can evaluate to True or False, 
+# and so long as the condition is True, the code in the loop will be executed. 
+# When the condition becomes False, the loop terminates. 
+
