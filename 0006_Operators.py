6db7472eba494aaa4cd13ee8b42b1171558aa30b
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-07-24 18:16:05 +0530

    Update 0006_Operators.py

--- 0006_Operators.py
+++ 0006_Operators.py
@@ -1,12 +1,12 @@
 a = 12
 b = 3
 
-print(a + b)   # 15
-print(a - b)   # 9
-print(a * b)   # 36
-print(a / b)   # 4.0
+print(a + b)  # 15
+print(a - b)  # 9
+print(a * b)  # 36
+print(a / b)  # 4.0
 print(a // b)  # 4 integer division rounded down towards -infinity
-print(a % b)   # 0 modulus gives the remainder after integer division
+print(a % b)  # 0 modulus gives the remainder after integer division
 
 # use case for integer division
 print()  # empty line
