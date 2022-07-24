c89d39540087117a32e7fcf94afe817591249975
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-07-24 18:07:54 +0530

    Update 0005_Numeric_data_types.py

--- 0005_Numeric_data_types.py
+++ 0005_Numeric_data_types.py
@@ -1,9 +1,13 @@
-#built in data types in python include numeric, iterator, sequence(also iterator), mapping, file, class and exception.
-#python has no type declaration.
-#python-2 had 4 but python3 has only 3 data types namely int, float and complex where int was made suffecient to replace the earlier int and long data types of python-2.
-
-#int data type is meant for integer, real num with no fractional part
-#float data type is meant for decimals, real num with some fractional part
-#this distinction was made between the categories because int computations aee performed significantly faster by computers in comprasion to float computation.
-#in python2 int has a limit of (2^31 -1); where as python 3 doesnt have limit for int.
-#in python the smallest and largest possibility for float no. is 2.22e-307 and 1.78e+308 with 52 digits of precision available.
+# built in data types in python include numeric, iterator, sequence(also iterator),
+# -mapping, file, class and exception.
+# python has no type declaration.
+# python-2 had 4 but python3 has only 3 data types namely int, float and complex where 
+# -int was made sufficient to replace the earlier int and long data types of python-2.
+ 
+# int data type is meant for integer, real num with no fractional part
+# float data type is meant for decimals, real num with some fractional part
+# this distinction was made between the categories because int computations are performed 
+# -significantly faster by computers in comparison to float computation.
+# in python2 int has a limit of (2^31 -1); whereas python 3 doesn't have limited int.
+# in python the smallest and largest possibility for float no. is 2.22e-307 
+# -and 1.78e+308 with 52 digits of precision available.
