d750f28e506851be829a759c61ff853427f33dc2
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-07-24 17:13:42 +0530

    Create 0018_PEP8_Design.py

---
+++ 0018_PEP8_Design.py
@@ -0,0 +1,5 @@
+# PEP-8, is a document that provides guidelines and best practices on how to write Python code.
+# The primary focus of PEP 8 is to improve the readability and consistency of Python code.
+
+# there are numerous rules and guide lines regarding numerous topics that can be accessed through the website
+# for instance, a guideline states that the code lines in python mustn't exceed 72 characters for consistency.
