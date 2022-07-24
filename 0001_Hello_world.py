2811a7f3b9a71ad453eee11edc5151e531db4fbb
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-07-24 17:30:17 +0530

    Update 0001_Hello_world.py

--- 0001_Hello_world.py
+++ 0001_Hello_world.py
@@ -4,7 +4,6 @@ print('Hello, World!')  # string literal
 print(1 + 2)            # calculation 
 print()                 # no arg; a function name must always be followed by () 
                         #-even if no arg is provided
-
 print("the end",
       "or is it",
       5+6)              # multiple arg at once
@@ -14,8 +13,6 @@ print("the end",
 # func is a block of code that can be called to execute, by its name
 # arg is the value that are passed to the func to work on, appears within the () 
 #-ahead of func name in func call statement
-
 # parameter is the var mentioned in func declaration to accept certain arg, 
 #-position same as arg but in func defination
-
 # return value is a value that a func leaves after execution
