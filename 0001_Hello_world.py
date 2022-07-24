e19b797c8588df7814a1e64918d75d4cbbe95f74
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-07-24 17:17:16 +0530

    Update 0001_Hello_world.py

--- 0001_Hello_world.py
+++ 0001_Hello_world.py
@@ -1,12 +1,12 @@
-#print function prints value of argument passed to it
-print('Hello, World!')  #string literal
-print(1 + 2)   #calculation 
-print()   #no arg; a function name must always be followed by () even if no arg is provided
-print("the end","or is it",5+6)   #multiple arg at once
+# print function prints value of argument passed to it
+print('Hello, World!')  # string literal
+print(1 + 2)   # calculation 
+print()   # no arg; a function name must always be followed by () even if no arg is provided
+print("the end","or is it",5+6)   # multiple arg at once
 
-#literal represents any kind of value 
-#string literal is a combination of characters available on the keyboard, enclosed by quotes
-#func is a block of code that can be called to execute, by its name
-#arg is the value that are passed to the func to work on, appears within the () ahead of func name in func call statement
-#parameter is the var mentioned in func declaration to accept certain arg, position same as arg but in func defination
-#return value is a value that a func leaves after execution
+# literal represents any kind of value 
+# string literal is a combination of characters available on the keyboard, enclosed by quotes
+# func is a block of code that can be called to execute, by its name
+# arg is the value that are passed to the func to work on, appears within the () ahead of func name in func call statement
+# parameter is the var mentioned in func declaration to accept certain arg, position same as arg but in func defination
+# return value is a value that a func leaves after execution
