b6f4a9c4c12428967e5f5ee93999c3525a7ea694
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-07-24 19:00:07 +0530

    Update 0011_Debugger_within_IDE.py

--- 0011_Debugger_within_IDE.py
+++ 0011_Debugger_within_IDE.py
@@ -1,14 +1,14 @@
-#instead of running the code, call for a debugger (wrt intellij IDE)
-#debug pane is opened insted of run pane and program runs usually
+# instead of running the code, call for a debugger (wrt intellij IDE)
+# debug pane is opened insted of run pane and program runs usually
 
-#Break points-
-##created and removed by clicking on the left number margin beside the target line
-##once created, stops the program to function beyond that line while debugging, allows us to take action
+# Break points-
+# #created and removed by clicking on the left number margin beside the target line
+# #once created, stops the program to function beyond that line while debugging, allows us to take action
 
-#inside debug pane several options in top horizontal tool bar-
-##1st-Show execution points: if clicked, takes you to the line of execution at the moment (ideal for large code)
-##2nd-Step over: if clicked moves to the next execution line in accordance to the code
-##8th-Evaluate expression:if clicked, allows us to evaluate exp. wrt the available var
+# inside debug pane several options in top horizontal tool bar-
+# #1st-Show execution points: if clicked, takes you to the line of execution at the moment (ideal for large code)
+# #2nd-Step over: if clicked moves to the next execution line in accordance to the code
+# #8th-Evaluate expression:if clicked, allows us to evaluate exp. wrt the available var
 
-#inside vertical toolbar-
-##4th-Stop: immediately terminates the program
+# inside vertical toolbar-
+# #4th-Stop: immediately terminates the program
