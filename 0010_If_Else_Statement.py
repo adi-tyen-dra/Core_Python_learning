7df26114c86c7e6cca0f48305a06c0d8760d7948
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-07-25 23:51:52 +0530

    Update 0010_If_Else_Statement.py

--- 0010_If_Else_Statement.py
+++ 0010_If_Else_Statement.py
@@ -1,4 +1,4 @@
-import random  # importing a module named random
+import random  # importing a module named random for the guessing game
 
 # flow of program and decision-making
 name = input("what is your name? ")
@@ -71,3 +71,39 @@ elif ans < guess:
         print("you've guessed correctly")
 else:
     print("bravo you did it!")
+
+# Challenge: Address the following two issues with the guessing game from if-else file:
+# 1-> let the answer be random at every attempt
+# 2-> let their be infinite attempts instead of two until the user guessed the number
+# 3-> provide an exit option if the user enters 0
+# solution-
+
+# when a builtin func is available in an external module/package 
+# -we must import it and use dot notation to use it in our program
+# it is suggested to import all modules and packages needed, 
+# -in the beginning of code though python allows inbetween import as well
+
+highest_limit = 10  # specified separately so that in case of change 
+# -there is no need to change it at different places inside the code
+answer = random.randint(1, highest_limit)  # using randint func from random module, 
+# -it produces random no. within the given inclusive range
+print(answer)  # TODO: delete after testing
+guess = 0  # initializing to any no. not in the range to enter once inside the loop
+print("please enter your guess between 1 to {}"
+      .format(highest_limit))
+print("incase of exit please enter 0")
+while guess != answer:
+    guess = int(input("->"))
+    if guess == 0:  # exit statement
+        print("sorry to see you go")
+        break
+    elif guess == answer:
+        print("you've guessed it, no. was indeed {}".format(guess))
+        break
+    elif guess > answer:
+        print("please try lower ")
+    else:
+        print("please try higher ")
+
+# Note: the TODO comment is highlighted differently in intellij ide 
+# -and helps the coder to keep a track of certain actions that might be needed in code
